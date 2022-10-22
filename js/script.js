187faa5f2a1f03fe23b2ee50b659c4b2d85e871b
Fares Aljawhary <FAljawhary@aspire-services.org>
2022-10-23 01:44:55 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,215 @@
+let navbar = document.querySelector('.navbar')
+
+document.querySelector('#menu-bar').onclick = () =>{
+    navbar.classList.toggle('active');
+}
+
+document.querySelector('#close').onclick = () =>{
+    navbar.classList.remove('active');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//header fixed
+window.onscroll = function(){
+
+    navbar.classList.remove('active');
+
+    if(window.scrollY >50){
+        document.querySelector('header').classList.add('active');
+    }else{
+        document.querySelector('header').classList.remove('active');
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//dark mode
+let themeToggler = document.querySelector('#theme-toggler');
+
+themeToggler.onclick = () =>{
+    themeToggler.classList.toggle('fa-sun');
+    if(themeToggler.classList.contains('fa-sun')){
+        document.querySelector('body').classList.add('active');
+    }else{
+        document.querySelector('body').classList.remove('active');
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//convert img color
+document.querySelectorAll('.small-image-1').forEach(images =>{
+    images.onclick = function(){
+        document.querySelector('.big-image-1').src = images.getAttribute('src');
+    }
+});
+
+document.querySelectorAll('.small-image-2').forEach(images =>{
+    images.onclick = function() {
+        document.querySelector('.big-image-2').src = images.getAttribute('src');
+    }
+});
+
+document.querySelectorAll('.small-image-3').forEach(images =>{
+    images.onclick = function(){
+        document.querySelector('.big-image-3').src = images.getAttribute('src');
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+let countDate = new Date('OCT 30, 2022 00:00:00').getTime();
+
+function countDown(){
+
+    let now = new Date().getTime();
+	gap = countDate - now;
+
+    let seconds = 1000;
+    let minutes = seconds * 60;
+    let hours = minutes * 60;
+    let days = hours * 24;
+
+    let d = Math.floor(gap / (days));
+	let h = Math.floor((gap % (days)) / (hours));
+	let m = Math.floor((gap % (hours)) / (minutes));
+	let s = Math.floor((gap % (minutes)) / (seconds));
+
+    document.getElementById('days').innerText = d;
+    document.getElementById('hours').innerText = h;
+    document.getElementById('minutes').innerText = m;
+    document.getElementById('seconds').innerText = s;
+
+}
+
+setInterval(function(){
+    countDown()
+},1000);
+
+var swiper = new Swiper(".product-slider", {
+    slidesPerView: 3,
+    loop:false,
+    spaceBetween: 10,
+    autoplay: {
+        delay: 4000
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 3,
+        },
+        550: {
+            slidesPerView: 2,
+        },
+        800: {
+            slidesPerView: 3,
+        },
+        1000: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+var swiper = new Swiper(".review-slider", {
+    slidesPerView: 3,
+    loop:true,
+    spaceBetween: 10,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        550: {
+            slidesPerView: 2,
+        },
+        800: {
+            slidesPerView: 3,
+        },
+        1000: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+
+
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace("active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += "active";
+    for(var i=1;i<=3;i++){
+        document.querySelector('#close_tab'+i).addEventListener('click',function(){
+            document.getElementById(cityName).style.display = "none";
+        })
+    }
+    
+    
+}
